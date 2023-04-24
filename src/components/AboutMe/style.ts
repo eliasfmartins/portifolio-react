c7d0d962f953102bc7cb39a28f1a8b2dd4f88198
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-04-24 09:30:25 -0300

    build

--- src/components/AboutMe/style.ts
+++ src/components/AboutMe/style.ts
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 export const AboutContainer = styled.main`
   width: 100vw;
-  height: calc(100vh - 100px);
+  height: calc(100vh - 80px);
   display: flex;
   flex-direction: column;
   justify-content: center;
-  color: ${({ theme }) => theme.sectuenario};
+  color: ${({ theme }) => theme.septenary};
 `;
--- src/components/page2/style.ts
+++ src/components/page2/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const SkillsContainer = styled.section`
   display: flex;
-  height: calc(100vh - 100px);
+  height: calc(100vh - 80px);
   justify-content: center;
   align-items: center;
   width: 100vw;
