1a9b211a2f751a134e7817759ee7a15386c59935
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-04-27 18:41:35 -0300

    ajustando queris

--- src/components/Home/styled.ts
+++ src/components/Home/styled.ts
@@ -40,7 +40,7 @@ export const HomeDysplayed = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    min-width: 380px;
+    min-width: 360px;
     max-width: 500px;
   }
   h2 {
