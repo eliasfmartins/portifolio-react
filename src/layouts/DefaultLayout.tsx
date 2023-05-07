12caaacda117dfbdc9426d83f11786a5360d5c11
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-07 17:09:35 -0300

    configurando layoutStyled

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer";
+import { LayoutContainer } from "./styled";
 export const DefaultLayout = () => {
   return (
-    <div>
+    <LayoutContainer>
       <Header colorHeader="light" isActive={false} />
       <Outlet />
       <Footer />
-    </div>
+    </LayoutContainer>
   );
 };
---
+++ src/layouts/styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  height: calc(100vh - 120px);
+  padding: 20px;
+`;
