48b011a75403da79d111abc07c898d62a85c950a
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-08 10:26:19 -0300

    ajustando versel

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,5 +1,4 @@
-import { Card } from "../Cards";
-import { Container, ShowContent, SkillsContainer } from "./styled";
+import { SkillsContainer } from "./styled";
 import { GrReactjs } from "react-icons/gr";
 import {
   SiCss3,
@@ -10,27 +9,20 @@ import {
 } from "react-icons/si";
 import { RiHtml5Fill } from "react-icons/ri";
 import { BsGit, BsGithub } from "react-icons/bs";
+import { Card } from "../Cards";
 
 export const Skills = () => {
   return (
-    <Container>
-      <SkillsContainer>
-        <Card title="React" img={<GrReactjs size={80} />} />
-        <Card title="JavaScript" img={<SiJavascript size={80} />} />
-        <Card title="TypeScript" img={<SiTypescript size={80} />} />
-        <Card title="Html" img={<RiHtml5Fill size={80} />} />
-        <Card title="CSS" img={<SiCss3 size={80} />} />
-        <Card title="Git " img={<BsGit size={80} />} />
-        <Card title="GitHub" img={<BsGithub size={80} />} />
-        <Card title="CSS Modules" img={<SiCssmodules size={80} />} />
-        <Card
-          title="Styled Conponents"
-          img={<SiStyledcomponents size={80} />}
-        />
-      </SkillsContainer>
-      <ShowContent>
-        <div className="center">Ainda em desenvolvimento</div>
-      </ShowContent>
-    </Container>
+    <SkillsContainer>
+      <Card title="react" img={<GrReactjs size={80} />} />
+      <Card title="JavaScript" img={<SiJavascript size={80} />} />
+      <Card title="TypeScript" img={<SiTypescript size={80} />} />
+      <Card title="Html" img={<RiHtml5Fill size={80} />} />
+      <Card title="CSS" img={<SiCss3 size={80} />} />
+      <Card title="Git " img={<BsGit size={80} />} />
+      <Card title="GitHub" img={<BsGithub size={80} />} />
+      <Card title="CSSModules" img={<SiCssmodules size={80} />} />
+      <Card title="StyledConponents" img={<SiStyledcomponents size={80} />} />
+    </SkillsContainer>
   );
 };
--- src/components/Skills/styled.ts
+++ src/components/Skills/styled.ts
@@ -4,42 +4,14 @@ export const Container = styled.div`
   flex-wrap: wrap;
 `;
 export const SkillsContainer = styled.div`
-  min-height: calc(100vh - 180px);
-  height: calc(100vh - 90px);
-  overflow-y: auto;
-  flex-wrap: wrap;
-  width: 50%;
-  padding: 20px;
+  /* height: calc(100vh - 90px); */
+
   margin-top: 60px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   gap: 2rem;
   align-items: center;
-  overflow: hidden;
-  @media (max-width: 968px) {
-    width: 90%;
-    height: auto;
-    align-items: center;
-    justify-content: center;
-  }
-`;
-export const ShowContent = styled.div`
-  width: 50%;
-  min-height: calc(100vh - 180px);
-  margin-top: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  div.center {
-    width: 20vw;
-    height: 20vw;
-    border-radius: 8px;
-    border: solid 2px white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
   @media (max-width: 768px) {
-    display: none;
   }
 `;
