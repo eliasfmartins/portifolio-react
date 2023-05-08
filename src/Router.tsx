a54924c903109afdedc3dfb6a72d25818b00e3d5
Elias F Martins <eliasmartinsdev8@gmail.com>
2023-05-08 10:30:02 -0300

    ajustando versel

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,10 +2,9 @@ import { Route, Routes, useLocation } from "react-router-dom";
 
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { AnimatePresence } from "framer-motion";
-import { Home} from "./components/Home";
+import { Home } from "./components/Home";
 import { ProjectPage } from "./components/ProjectPage";
 import { Skills } from "./components/Skills";
-
 
 export const Router = () => {
   const location = useLocation();
@@ -14,7 +13,7 @@ export const Router = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/projectpage" element={<ProjectPage/>} />
+          <Route path="/projectpage" element={<ProjectPage />} />
           <Route path="/skills" element={<Skills />} />
         </Route>
       </Routes>
